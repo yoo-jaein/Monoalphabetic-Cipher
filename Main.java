6bdae149f0345f7fd96525dfb704e1a5f929a98e
Jaein Yoo <y_jaein@naver.com>
2020-09-28 21:00:43 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,24 +9,25 @@ public class Main {
 	public static void makeKey() {
 		try {
 			// 임의의 key 생성
-		    OutputStream output = new FileOutputStream("C:/project/Key.txt");
-		    StringBuilder sb = new StringBuilder();
-		    
-		    Random r = new Random();
+			OutputStream output = new FileOutputStream("C:/project/Key.txt");
+			StringBuilder sb = new StringBuilder();
+			
+			Random r = new Random();
 			String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		    for(int i=0; i<26; i++) {
-		    	char c = abc.charAt(r.nextInt(abc.length()));
-		    	
-		    	if(sb.toString().contains(String.valueOf(c)))
-		    		i--;
-		    	else
-		    		sb.append(c);
-		    }
-		    
-		    // 파일로 저장
-		    byte[] by = sb.toString().getBytes();
-		    output.write(by);
-		    output.close();
+
+			for(int i=0; i<26; i++) {
+				char c = abc.charAt(r.nextInt(abc.length()));
+
+				if(sb.toString().contains(String.valueOf(c)))
+					i--;
+				else
+					sb.append(c);
+			}
+
+			// 파일로 저장
+			byte[] by = sb.toString().getBytes();
+			output.write(by);
+			output.close();
 		} catch (Exception e) {
 			e.getStackTrace();
 		}
@@ -37,49 +38,49 @@ public class Main {
 			// key 파일 읽기
 			File keyFile = new File("C:/project/Key.txt");
 			FileReader file_reader = new FileReader(keyFile);
-			
+
 			StringBuilder sb = new StringBuilder();
-			
+
 			int cur = 0;
 			while((cur = file_reader.read())!=-1){
 				sb.append((char)cur);
 			}
-			
+
 			String key = sb.toString();
 			String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			
-			
+
+
 			// 원문 텍스트 파일 읽기
 			File plainFile = new File("C:/project/Plaintext.txt");
 			FileReader file_reader2 = new FileReader(plainFile);
-			
+
 			StringBuilder sb2 = new StringBuilder();
-			
+
 			int cur2 = 0;
 			while((cur2 = file_reader2.read())!=-1){
 				sb2.append(Character.toUpperCase((char)cur2));
 			}
-			
+
 			String plainText = sb2.toString();
-			
-			
+
+
 			// 암호문 텍스트 파일 생성 
-		    OutputStream output = new FileOutputStream("C:/project/Ciphertext.txt");
-		    StringBuilder sb3 = new StringBuilder();
-		    
-		    for(int i=0; i<plainText.length(); i++) {
-		    	char c = plainText.charAt(i);
-		    	if(Character.isAlphabetic(c))
-		    		sb3.append(key.charAt(abc.indexOf(c)));
-		    	else
-		    		sb3.append(c);
-		    }
-		    
-		    
-		    // 파일로 저장
-		    byte[] by = sb3.toString().getBytes();
-		    output.write(by);	
-			
+			OutputStream output = new FileOutputStream("C:/project/Ciphertext.txt");
+			StringBuilder sb3 = new StringBuilder();
+
+			for(int i=0; i<plainText.length(); i++) {
+				char c = plainText.charAt(i);
+				if(Character.isAlphabetic(c))
+					sb3.append(key.charAt(abc.indexOf(c)));
+				else
+					sb3.append(c);
+			}
+
+
+			// 파일로 저장
+			byte[] by = sb3.toString().getBytes();
+			output.write(by);	
+
 			file_reader.close();
 			file_reader2.close();
 			output.close();
@@ -97,49 +98,49 @@ public class Main {
 			// key 파일 읽기
 			File keyFile = new File("C:/project/Key.txt");
 			FileReader file_reader = new FileReader(keyFile);
-			
+
 			StringBuilder sb = new StringBuilder();
-			
+
 			int cur = 0;
 			while((cur = file_reader.read())!=-1){
 				sb.append((char)cur);
 			}
-			
+
 			String key = sb.toString();
 			String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			
-			
+
+
 			// 암호문 텍스트 파일 읽기
 			File cipherFile = new File("C:/project/Ciphertext.txt");
 			FileReader file_reader2 = new FileReader(cipherFile);
-			
+
 			StringBuilder sb2 = new StringBuilder();
-			
+
 			int cur2 = 0;
 			while((cur2 = file_reader2.read())!=-1){
 				sb2.append(Character.toUpperCase((char)cur2));
 			}
-			
+
 			String cipherText = sb2.toString();
-			
-			
+
+
 			// 원문 텍스트 파일 생성
-		    OutputStream output = new FileOutputStream("C:/project/Plaintext2.txt");
-		    StringBuilder sb3 = new StringBuilder();
-		    
-		    for(int i=0; i<cipherText.length(); i++) {
-		    	char c = cipherText.charAt(i);
-		    	if(Character.isAlphabetic(c))
-		    		sb3.append(abc.charAt(key.indexOf(c)));
-		    	else
-		    		sb3.append(c);
-		    }
-		    
-		    
-		    // 파일로 저장
-		    byte[] by = sb3.toString().getBytes();
-		    output.write(by);
-			
+			OutputStream output = new FileOutputStream("C:/project/Plaintext2.txt");
+			StringBuilder sb3 = new StringBuilder();
+
+			for(int i=0; i<cipherText.length(); i++) {
+				char c = cipherText.charAt(i);
+				if(Character.isAlphabetic(c))
+					sb3.append(abc.charAt(key.indexOf(c)));
+				else
+					sb3.append(c);
+			}
+
+
+			// 파일로 저장
+			byte[] by = sb3.toString().getBytes();
+			output.write(by);
+
 			file_reader.close();
 			file_reader2.close();
 			output.close();
